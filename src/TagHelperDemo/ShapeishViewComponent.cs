fe1480894a226cef6b771ab7d9d01d226316c36d
N. Taylor Mullen <nimullen@microsoft.com>
2015-09-29 21:11:10 -0700

    Finished ShapeishTagHelper.

---
+++ src/TagHelperDemo/ShapeishViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace TagHelperDemo
+{
+    public class ShapeishViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string name, int age)
+        {
+            dynamic model = new ExpandoObject();
+
+            model.Name = name;
+            model.Age = age;
+
+            return View("/Views/Shapeish", model);
+        }
+    }
+}
---
+++ src/TagHelperDemo/TagHelpers/ShapeishTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TagHelperDemo.TagHelpers
+{
+    public class ShapeishTagHelper : TagHelper
+    {
+        private readonly IViewComponentHelper _viewComponentHelper;
+
+        public ShapeishTagHelper(IViewComponentHelper viewComponentHelper)
+        {
+            _viewComponentHelper = viewComponentHelper;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            ((ICanHasViewContext)_viewComponentHelper).Contextualize(ViewContext);
+
+            output.Content.SetContent(_viewComponentHelper.Invoke("Shapeish", Name, Age));
+
+            output.TagName = null;
+        }
+    }
+}
